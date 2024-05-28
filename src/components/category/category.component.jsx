cdd30a25fd72569a1834de63975164b6be9e5e52
Mateus Felipe Colla <mateus@jettacargo.com.br>
2024-05-28 16:06:40 -0300

    test

--- src/components/category/category.component.jsx
+++ src/components/category/category.component.jsx
@@ -2,7 +2,7 @@ import "./category.styles.scss";
 
 const CategoryItem = ({ category }) => {
     return (
-        <div className="c-category" key={category.id} style={{
+        <div className="c-category" key={category.id} style={{ 
             backgroundImage: `url(${category.imageUrl})`
           }} >
             <h2>{category.name}</h2>
