ec18e2a91f1b6ded135a88f84efac480230c0c88
Mateus Felipe Colla <felipe.colla.m@gmail.com>
2024-06-04 13:42:27 -0300

    feat: Add user authentication functionality
    The code changes include adding user authentication functionality to the login component. This allows users to sign in with their Google account and creates a user document in the Firestore database if it doesn't already exist.

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,13 @@ import {
     GoogleAuthProvider
 } from 'firebase/auth';
 
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore';
+
 const firebaseConfig = {
     apiKey: "AIzaSyBlk4Ruht5dgXjx9CS8TAnLYEhSh7UAghI",
     authDomain: "capstone-clothing-db-cdede.firebaseapp.com",
@@ -25,3 +32,27 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore(firebaseApp);
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    const userSnapshot = await getDoc(userDocRef)
+
+    if(!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            })
+        } catch(e) {
+            console.log("Error creating user document: ", e.message);
+        }
+    }
+
+    return userDocRef;
+}
